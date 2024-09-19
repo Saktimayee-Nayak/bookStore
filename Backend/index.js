c9a9761cb7d5962629de7e041489d62467832609
Saktimayee Nayak <131857334+Saktimayee-Nayak@users.noreply.github.com>
2024-09-19 08:42:19 +0530

    add some file

--- Backend/index.js
+++ Backend/index.js
@@ -5,11 +5,14 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import bookRoute from "./route/book.route.js";
 import cors from 'cors';
+import userRoute from "./route/user.route.js";
 
 const app = express()
 // const port = 3000
 
 app.use(cors());
+app.use(express.json());
+
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
@@ -28,6 +31,7 @@ try {
 
 //defining routes
 app.use("/book", bookRoute)
+app.use("/user", userRoute)
 
 
 
